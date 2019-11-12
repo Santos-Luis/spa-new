6daf994e977f52a75bd4f02dd1a0bcccd5139806
Luís Henriques <luis.henriques@uniplaces.com>
2019-11-12 14:00:21 +0000

    Add missing section on header

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-remove-trailing-slashes`,
+    `gatsby-plugin-catch-links`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -6,12 +6,42 @@ import UniplacesLogo from "../../assets/images/uniplaces-logo.svg";
 const Header = () => {
   return(
     <header className={styles.header}>
-      <Link to="/">
+      <Link to="/" className={styles.header__elements} key="logo">
         <UniplacesLogo />
       </Link>
-      <div className={styles.header__right}>
-        123
-      </div>
+      <ul>
+        <li key="how-it-works">
+          <a href="https://find.uniplaces.com/how-it-works/">
+            How it works
+          </a>
+        </li>
+        <li key="list-your-place">
+          <a href="http://rent.staging-uniplaces.com">
+            List your place
+          </a>
+        </li>
+        <li key="assisted-booking">
+          <a href="https://find.uniplaces.com/find-accommodation-europe/">
+            Assisted booking
+          </a>
+        </li>
+        <li key="help">
+          <a href="https://help.uniplaces.com/help/s">
+            Help
+          </a>
+        </li>
+        <li key="log-in">
+          <a href="#">
+            Log in
+          </a>
+        </li>
+        <li key="sign-up">
+          <a href="#">
+            Sign up
+          </a>
+        </li>
+      </ul>
+      
     </header>
   );
 }
